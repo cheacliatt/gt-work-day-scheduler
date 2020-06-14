bf36f1f627c0195518a12013f2c139a6def44c20
CheaC <cheacliatt@gmail.com>
2020-06-14 13:24:37 -0400

    Everything is red. This is fine.

--- script.js
+++ script.js
@@ -1,61 +1,57 @@
 $(document).ready(function () {
   var arrayOfHours = [
-    "0000",
-    "0100",
-    "0200",
-    "0300",
-    "0400",
-    "0500",
-    "0600",
-    "0700",
-    "0800",
-    "0900",
-    "1000",
-    "1100",
-    "1200",
-    "1300",
-    "1400",
-    "1500",
-    "1600",
-    "1700",
-    "1800",
-    "1900",
-    "2000",
-    "2100",
-    "2200",
-    "2300",
-    "2400",
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
   ];
 
+  var currentTime = moment().format("h A");
+
+
   // var hourIndex = 0;
+
+  // var currentHour = moment().format(HH);
 
   // Populate a row for each hour, then set up three columns in each row
   // the first column should be a class of "col-sm-1" and contain an hour
   // the second column should be a class of "col-sm-10" and contain a input text area
   // the third column should be a class of "col-sm-1" and have a button that will save the text on local storage
 
-  // var currentTime = moment();
-  // $("#currentDay").text.(currentTime);
 
-  moment('2010-10-20').isAfter('2010-10-19'); // true
 
   for (var i = 0; i < arrayOfHours.length; i++) {
     console.log(arrayOfHours[i]);
     // var currentHour = moment().format("HHmm")
     var hourList = arrayOfHours[i];
-    var timeTableRows = $("<div class='row form-group hourRows'>");
+    var timeTableRows = $("<div class='row form-group hour-rows'>");
     var hourEl = $("<div class='col-sm-1'>");
     var textEl = $("<textarea class='col-sm-10 time-sensitive form-control'>");
-    var saveEl = $("<button type='button' class='col-sm-1 btn btn-info'>");
+    var saveEl = $("<button type='button' class='col-sm-1 btn btn-info i fas fa-lock'>");
     hourEl.text(hourList);
     timeTableRows.append(hourEl).append(textEl).append(saveEl);
     $("#time-table").append(timeTableRows);
+
+    if (hourList.match(currentTime)) {
+      $(".time-sensitive").attr("style", "background-color: red");
+    }
   }
+
+  $(".btn").on("click", function() {
+    console.log("You clicked my button!");
+  });
+
 
 
   var maybe = moment().isAfter(hourList);
   console.log(maybe);
     // If a row of time is after the current time in the time-table, it should turn green 
+    // Look at calculator example
 
   // if(moment().isAfter(hourList) === true ) {
   //   $(".time-sensitive").attr('style',  'background-color:green');
@@ -65,10 +61,18 @@ $(document).ready(function () {
 // Apply moment attributes to each hour
 // Make the rows change color based off which hour it is
 
+// var currentHourTime = moment().format(HH);
+
+//   if (moment().format(HH) === true) {
+//     $(".time-sensitive").attr("style", "background-color: red");
+//   }
 
 
-var currentTime = moment().format("dddd Do MMMM YYYY")
+var currentTime = moment().format("dddd Do MMMM YYYY");
 
-console.log(currentTime);
+// console.log(currentTime);
 
 $("#currentDay").text(currentTime);
+
+var milTime = moment().format("HHA");
+console.log(milTime);
