01c8d34a33862220706d9340c2d7cb8adf1de3af
CheaC <cheacliatt@gmail.com>
2020-06-15 19:26:33 -0400

    Everything works.

--- script.js
+++ script.js
@@ -1,75 +1,76 @@
 $(document).ready(function () {
   var arrayOfHours = [
-    "9 AM",
-    "10 AM",
-    "11 AM",
-    "12 PM",
-    "1 PM",
-    "2 PM",
-    "3 PM",
-    "4 PM",
-    "5 PM",
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM",
   ];
 
-  var emptyBox = [];
+  var currentTime = moment().format("hA");
+  var currentTimeIndex = arrayOfHours.indexOf(currentTime);
 
-  var currentTime = moment().format("h A");
+  var currentTimeHeader = moment().format("dddd Do MMMM YYYY");
+
+
+  $("#currentDay").text(currentTimeHeader);
 
   // Populate a row for each hour, then set up three columns in each row
   // the first column should be a class of "col-sm-1" and contain an hour
   // the second column should be a class of "col-sm-10" and contain a input text area
   // the third column should be a class of "col-sm-1" and have a button that will save the text on local storage
 
-
-
   for (var i = 0; i < arrayOfHours.length; i++) {
-    console.log(arrayOfHours[i]);
+    // console.log(arrayOfHours[i]);
     // var currentHour = moment().format("HHmm")
     var hourList = arrayOfHours[i];
-    var timeTableRows = $("<section class='row form-group hour-rows'>");
-    var hourEl = $("<div class='col-sm-1 hour-col'>");
-    var textEl = $("<textarea class='col-sm-10 time-sensitive form-control'>");
-    var saveEl = $("<button type='submit' class='col-sm-1 btn btn-info i fas fa-lock'>");
+    var timeTableRows = $("<form class='row form-group hour-rows'>");
+    var hourEl = $("<div class='col-sm-1 hour-col hour'>");
+    var textEl = $("<textarea class='col-sm-10 time-sensitive form-control time-block'>");
+    var saveEl = $(
+      "<button type='submit' class='col-sm-1 btn btn-info i fas fa-lock saveBtn'>"
+    );
     hourEl.text(hourList);
-    textEl.addClass(arrayOfHours[i]);
+    textEl.attr("id", arrayOfHours[i]);
     timeTableRows.append(hourEl).append(textEl).append(saveEl);
     $("#time-table").append(timeTableRows);
 
-    if (hourList.match(currentTime)) {
+    if (i === currentTimeIndex) {
       $(textEl).addClass("present");
-    } else if (hourList < currentTime) {
+    } else if (i < currentTimeIndex || currentTimeIndex === -1) {
       $(textEl).addClass("past");
     } else {
       $(textEl).addClass("future");
     }
-  };
+  }
 
-  $(".btn").on("click", "section", function() {
-    // event.preventDefault();
-    var entryEl = $(".11").val();
-    var entryEl12 = $(".12").val();
-    emptyBox.push(entryEl);
-    emptyBox.push(entryEl12)
-    console.log(entryEl);
-    console.log(entryEl12);
 
-    if (emptyBox.length > 0) {
-      console.log("You submitted the form.")
-    }
+
+  $(document).on("click", ".btn", function (event) {
+    event.preventDefault();
+    var textValue = $(this).siblings("textarea").val();
+    var textKey = $(this).siblings("textarea").attr("id");
+    console.log(textKey, textValue);
+    localStorage.setItem(textKey, textValue);
   });
+  $("#9AM").val(localStorage.getItem("9AM"));
+  $("#10AM").val(localStorage.getItem("10AM"));
+  $("#11AM").val(localStorage.getItem("11AM"));
+  $("#12PM").val(localStorage.getItem("12PM"));
+  $("#1PM").val(localStorage.getItem("1PM"));
+  $("#2PM").val(localStorage.getItem("2PM"));
+  $("#3PM").val(localStorage.getItem("3PM"));
+  $("#4PM").val(localStorage.getItem("4PM"));
+  $("#5PM").val(localStorage.getItem("5PM"));
 
 });
-
 
 // if(document.getElementById("field").value == '')
 // {
 //     alert("debug");
 //     document.getElementById("field").style.display ="none";
 // }
-
-
-var currentTime = moment().format("dddd Do MMMM YYYY");
-
-// console.log(currentTime);
-
-$("#currentDay").text(currentTime);
